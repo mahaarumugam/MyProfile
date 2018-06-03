c3b7b2de3825334526b0ad06e528c52cec00701d
mahaarumugam <39696057+mahaarumugam@users.noreply.github.com>
2018-06-03 17:18:02 +0530

    Update MahaProfile1.java

--- src/com/personal/MahaProfile1.java
+++ src/com/personal/MahaProfile1.java
@@ -1,5 +1,17 @@
 package com.personal;
 
-public class MahaProfile1 {
+public class MahaProfile1 implements IProfile{
+@Override
+	public void myBasicInfo() {
+		// TODO Auto-generated method stub
+		System.out.println("FirstName : Maha");
+		System.out.println("LastName : Eswari");
+		System.out.println("Age : 21");
+	}
 
+	@Override
+	public void myHobbies() {
+		// TODO Auto-generated method stub
+		System.out.println("My hobbies is watching news and reading history books");
+	}
 }
